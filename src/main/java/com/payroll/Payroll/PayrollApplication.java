c2c98ba56ee4c010795c2b3cfb82e467952c2157
AnudeepDVS <saianudeep242@gmail.com>
2025-06-27 02:36:48 +0530

    initial commit CRUD project

---
+++ src/main/java/com/payroll/Payroll/PayrollApplication.java
@@ -0,0 +1,20 @@
+package com.payroll.Payroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+@EnableWebMvc
+public class PayrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayrollApplication.class, args);
+	}
+
+
+
+
+
+}
